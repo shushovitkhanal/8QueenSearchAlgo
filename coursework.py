886cbf3ec878991e0fc6e046f38f79db8109cae7
shushovitkhanal <shushovitkhanal@gmail.com>
2024-03-03 18:33:03 +0000

    chessboard implemented correctly

--- coursework.py
+++ coursework.py
@@ -9,15 +9,26 @@ def is_even(number) \
 def chess_board():
     height = 8
     width = 8
+
+    x_coord_list = ['A','B','C','D','E','F','G','H']
+    is_xcoord_printed = False
+
     for i in range(height):
         for j in range(width):
 
+            if not is_xcoord_printed:
+                for item in x_coord_list:
+                    print(' ' + item, end=' ')
+                print()
+                is_xcoord_printed = True
             block = (i + j) % 2
             if block == 1:
-                print(u"\u2B1B", end=' ')
+                print("\033[7m   ", end="\033[0m")
             else:
-                print(u"\u2B1C", end=' ')
+                print("\033[0m   ", end='')
 
+            if j == 8:
+                print("   ")
         print()
 
 
