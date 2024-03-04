d0f1d0b3d637732127612c89308b430232787f73
shushovitkhanal <shushovitkhanal@gmail.com>
2024-03-04 17:16:46 +0000

    Fixed the misaligned board

--- coursework.py
+++ coursework.py
@@ -1,17 +1,10 @@
-def is_even(number) \
-        :
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-
 
 class Chessboard:
 
     def __int__(self, x, y):
 
-        board_height = 8
-        board_width = 8
+        self.board_height = 8
+        self.board_width = 8
 
         self.queen_placement = [
             ['', '', '', '', '', '', '', ''],
@@ -25,12 +18,14 @@ class Chessboard:
         ]
         self.x_unmovable = x
         self.y_unmovable = y
+
         self.place_queen(x, y)
+        self.placeFirstQueen = True
 
     def place_queen(self, x, y):
         x_coord = x - 1
-        y_coord = y - 1
-        self.queen_placement[x_coord][y_coord] = "Q"
+        y_coord = self.board_height - y
+        self.queen_placement[y_coord][x_coord] = "Q"
 
     def remove_queen(self, x, y):
         x_coord = x - 1
@@ -44,7 +39,6 @@ class Chessboard:
             return True
         else:
             return False
-
     def print_chessboard(self):
         height = 8
         width = 8
@@ -56,7 +50,6 @@ class Chessboard:
             for j in range(1, width + 1):
 
                 is_queen_on_tile = self.get_queen(i, j)
-
 
                 if not is_cord_printed:
                     print('', end='  ')
@@ -71,45 +64,31 @@ class Chessboard:
                 block = (i + j) % 2
                 if block == 1:
                     if is_queen_on_tile:
-                        print("\033[0m \u265B ", end="")
+                        if self.placeFirstQueen:
+                            print("\033[0m Q ", end="")
+                            self.placeFirstQueen = False
+                        else:
+                            print("\033[0m x ", end="")
                     else:
                         print("\033[0m   ", end="")
                 else:
-                    print("\033[7m   ", end="\033[0m")
+                    if is_queen_on_tile:
+                        if self.placeFirstQueen:
+                            print("\033[7m Q ", end="\033[0m")
+                            self.placeFirstQueen = False
+                        else:
+                            print("\033[7m x ", end="\033[0m")
+                    else:
+                        print("\033[7m   ", end="\033[0m")
 
                 if j == 9:
                     print("   ")
             print()
 
 
-'''def chess_board():
-    height = 8
-    width = 8
-
-    x_coord_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    is_cord_printed = False
-
-    for i in range(height):
-        for j in range(width):
-
-            if not is_cord_printed:
-                for item in x_coord_list:
-                    print(' ' + item, end=' ')
-                print()
-                is_cord_printed = True
-
-            block = (i + j) % 2
-            if block == 1:
-                print("\033[7m   ", end="\033[0m")
-            else:
-                print("\033[0m   ", end='')
-
-            if j == 8:
-                print("   ")
-        print()
-'''
-
 if __name__ == '__main__':
     board = Chessboard.__new__(Chessboard)
-    board.__int__(4, 1)
+    board.__int__(4, 8)
+    board.place_queen(2,4)
+    board.place_queen(8,3)
     board.print_chessboard()
